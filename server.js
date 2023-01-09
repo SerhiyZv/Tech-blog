77dc97c88886f3f4d1ee7b4828ce64d65c6e092e
SerhiyZv <skiev31@gmail.com>
2023-01-08 23:44:41 -0500

    Add homeRoutes and index.js to controllers folder

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-
-
+const exphbs = require('express-handlebars');
+const router = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/auth');
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(router);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on PORT ${PORT}'));
