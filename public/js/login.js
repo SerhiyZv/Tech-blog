c64f83b9285647e4a51c1e28a3b345ebcdda42d7
SerhiyZv <skiev31@gmail.com>
2023-01-11 17:48:54 -0500

    Add login.js to display login form

---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login");
+
+const logUserIn = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#loginEmail").value.trim();
+    const password = document.querySelector("#loginPassword").value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: "POST",
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert("Email or password not recognized. Please try again.")
+        }
+
+    } else if (!email && !password) {
+        alert("Enter email and password");
+    } else if (!email) {
+        alert("Enter email");
+    } else if (!password) {
+        alert("Enter password");
+    }
+}
+
+loginForm.addEventListener("submit", logUserIn);
