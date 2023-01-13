37dac0b1ceeee62d328e1549873f849d2466a481
SerhiyZv <skiev31@gmail.com>
2023-01-12 23:17:03 -0500

    Update connection with JawsDB variables

--- config/connection.js
+++ config/connection.js
@@ -7,16 +7,22 @@ require('dotenv').config();
 // Destructure values from .env file for SQL server connection
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
+let sequelize;
+
 // Establish SQL server through Sequelize
-const sequelize = new Sequelize(
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
