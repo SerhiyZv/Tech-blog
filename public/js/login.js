9d30e3b2c7405f9726e4a68b102d0634cc953ee7
SerhiyZv <skiev31@gmail.com>
2023-01-14 20:16:20 -0500

    Add validation for sign-up input

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const logUserIn = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert("Email or password not recognized. Please try again.")
         }
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,19 @@
+const { ValidationError } = require("sequelize");
+
 const signupForm = document.querySelector("#signup");
+const passwordField = document.querySelector("#signupPassword");
+const verifyField = document.querySelector("#verifyPassword");
+const submitButton = document.querySelector("#submitButton");
 
 const registerUser = async (event) => {
     event.preventDefault();
 
     const username = document.querySelector("#signupUsername").value.trim();
     const email = document.querySelector("#signupEmail").value.trim();
-    const password = document.querySelector("#signupPassword").value.trim();
+    
+    const password = passwordField.value.trim();
 
-    if (username && email && password) {
-        if (password.length < 8) {
-            alert("Password must be at least 8 characters long");
-            return;
-        }
-
+    if (validate()) {
         const response = await fetch('/api/users/signup', {
             method: "POST",
             body: JSON.stringify({ 
@@ -33,4 +34,56 @@ const registerUser = async (event) => {
     }
 }
 
+const checkLength = () => {
+    const password = passwordField.value.trim();
+    const checkbox1 = document.querySelector("#checkbox1");
+
+    if (password.length >= 8) {
+        checkbox1.classList.remove("hide");
+        return true;
+    } else {
+        checkbox1.classList.add("hide");
+        return false;
+    }
+}
+
+const verifyPassword = () => {
+    // Main password field
+    const password1 = passwordField.value.trim();
+
+    // Verify password field
+    const password2 = verifyField.value.trim();
+
+    const checkbox2 = document.querySelector("#checkbox2");
+
+    if (password1 === password2) {
+        checkbox2.classList.remove("hide");
+        return true;
+    } else {
+        checkbox2.classList.add("hide");
+        return false;
+    }
+}
+
+const validate = () => {
+    const username = document.querySelector("#signupUsername").value.trim();
+    const email = document.querySelector("#signupEmail").value.trim();
+
+    if (
+        username.length > 0 &&
+        email.length > 0 &&
+        checkLength() &&
+        verifyPassword()
+    ) {
+        submitButton.removeAttribute("disabled");
+        return true;
+    } else {
+        submitButton.setAttribute("disabled", "");
+        return false;
+    }
+}
+
+passwordField.addEventListener("input", checkLength);
+verifyField.addEventListener("input", verifyPassword);
+signupForm.addEventListener("input", validate);
 signupForm.addEventListener("submit", registerUser);
